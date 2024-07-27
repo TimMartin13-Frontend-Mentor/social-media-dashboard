6ff13abe198d1e8d868eaf0b4669bbc6a682073b
Tim Martin <timmartin13@gmail.com>
2024-07-27 14:56:11 -0500

    Fix Vite.comfig

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        profileData: resolve(__dirname, 'profileData.json'),
       },
     },
   },
